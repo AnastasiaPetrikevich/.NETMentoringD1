43b94b2c1e68e5bf783da306f7e0ecdbf4f35370
Anastasiya Petrykevich <Anastasiya.Petrykevich@thomsonreuters.com>
2020-03-03 13:28:24 +0300

    Added ExceptionHandling (Task 1)

---
+++ 02. C# Fundamentals/ExceptionHandling/FirstSymbol/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstSymbol
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please, enter string: ");
+
+			while (Console.ReadKey(true).Key != ConsoleKey.Escape)
+			{
+				string sourceString = Console.ReadLine();
+
+				if (string.IsNullOrWhiteSpace(sourceString))
+				{
+					Console.WriteLine("You entered string is null, empty or white space.");
+					continue;
+				}
+				
+				Console.WriteLine($"First character: '{sourceString[0]}'.");
+				
+			}
+
+		}
+	}
+}
