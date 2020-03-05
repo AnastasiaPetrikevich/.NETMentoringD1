11794f6ddad30bc918b18d29698f6c2ba7e46a09
Alex <alex.bely.420601@gmail.com>
2020-03-05 23:32:57 +0300

    Fix #2

--- 02. C# Fundamentals/Advanced CSharp/FileSystemVisitorConsoleUI/Program.cs
+++ 02. C# Fundamentals/Advanced CSharp/FileSystemVisitorConsoleUI/Program.cs
@@ -15,69 +15,67 @@ namespace FileSystemVisitorConsoleUI
 		{
 			//Console.WriteLine("Please, enter path:");
 			//string searchPath = Console.ReadLine();
-			//string searchPath = @"C:\Windows";
-			string searchPath = @"D:\Temp";
+			string searchPath = @"D:\.NETMentoringD1";
 
 			#region All files and directories
 
 			Console.WriteLine("All files and directories");
 			var visitor = new FileSystemVisitor(new VisitorService());
-			//visitor.OnStart += (s, e) => Console.WriteLine("Search started...");
-			//visitor.OnFinish += (s, e) => Console.WriteLine("Search finished.");
-			//visitor.OnDirectoryFinded += (s, e) => Console.WriteLine($"Directory: {e.FileSystemInfo.FullName}");
-			//visitor.OnFileFinded += (s, e) => Console.WriteLine($"File: {e.FileSystemInfo.FullName}");
+			visitor.OnStart += (s, e) => Console.WriteLine("Search started...");
+			visitor.OnFinish += (s, e) => Console.WriteLine("Search fineshed.");
+			visitor.OnDirectoryFinded += (s, e) => Console.WriteLine($"Directory: {e.FileSystemInfo.FullName}");
+			visitor.OnFileFinded += (s, e) => Console.WriteLine($"File: {e.FileSystemInfo.FullName}");
 
 			Console.WriteLine("Results 1:");
 
 			foreach (FileSystemInfo entry in visitor.Search(searchPath))
 			{
-				Console.WriteLine($"{entry.FullName}");
 			}
 
 			#endregion
 
 
-			//#region Exclude files without filter
+			#region Exclude files without filter
 
-			//Console.WriteLine("Exclude files without filter");
-			//visitor = new FileSystemVisitor(new VisitorService());
-			//visitor.OnStart += (s, e) => Console.WriteLine("Search started...");
-			//visitor.OnFinish += (s, e) => Console.WriteLine("Search finished.");
-			//visitor.OnFileFinded += (s, e) =>
-			//{
-			//	if (e.FileSystemInfo.Name.Contains("csp"))
-			//	{
-			//		e.Status = SearchStatus.Exclude;
-			//	}
-			//	else
-			//	{
-			//		Console.WriteLine($"File: {e.FileSystemInfo.FullName}");
-			//	}
-			//};
+			Console.WriteLine("Exclude files without filter");
+			visitor = new FileSystemVisitor(new VisitorService());
+			visitor.OnStart += (s, e) => Console.WriteLine("Search started...");
+			visitor.OnFinish += (s, e) => Console.WriteLine("Search fineshed.");
+			visitor.OnFileFinded += (s, e) =>
+			{
+				if (e.FileSystemInfo.Name.Contains("csp"))
+				{
+					e.Status = SearchStatus.Exclude;
+				}
+				else
+				{
+					Console.WriteLine($"File: {e.FileSystemInfo.FullName}");
+				}
+			};
 
-			//Console.WriteLine("Results 2:");
+			Console.WriteLine("Results 2:");
 
-			//foreach (FileSystemInfo entry in visitor.Search(searchPath))
-			//{
-			//}
+			foreach (FileSystemInfo entry in visitor.Search(searchPath))
+			{
+			}
 
-			//#endregion
+			#endregion
 
-			//#region Filtered files
+			#region Filtered files
 
-			//Console.WriteLine("Filtered files");
-			//visitor = new FileSystemVisitor(new VisitorService(), (path) => path.Name.Contains(".csproj"));
-			//visitor.OnStart += (s, e) => Console.WriteLine("Search started...");
-			//visitor.OnFinish += (s, e) => Console.WriteLine("Search finished.");
-			//visitor.OnFilteredFileFinded += (s, e) => Console.WriteLine($"Filtered file: {e.FileSystemInfo.FullName}");
+			Console.WriteLine("Filtered files");
+			visitor = new FileSystemVisitor(new VisitorService(), (path) => path.Name.Contains(".csproj"));
+			visitor.OnStart += (s, e) => Console.WriteLine("Search started...");
+			visitor.OnFinish += (s, e) => Console.WriteLine("Search fineshed.");
+			visitor.OnFilteredFileFinded += (s, e) => Console.WriteLine($"Fitered file: {e.FileSystemInfo.FullName}");
 
-			//Console.WriteLine("Results 3:");
+			Console.WriteLine("Results 3:");
 
-			//foreach (FileSystemInfo entry in visitor.Search(searchPath))
-			//{
-			//}
+			foreach (FileSystemInfo entry in visitor.Search(searchPath))
+			{
+			}
 
-			//#endregion
+			#endregion
 
 			Console.ReadKey();
 		}
--- 02. C# Fundamentals/Advanced CSharp/FileSystemVisitorLogic/Services/FileSystemVisitor.cs
+++ 02. C# Fundamentals/Advanced CSharp/FileSystemVisitorLogic/Services/FileSystemVisitor.cs
@@ -10,8 +10,10 @@ namespace FileSystemVisitorLogic.Services
 {
 	public class FileSystemVisitor
 	{
-		private readonly IVisitorService _visitorService;
+		private readonly IVisitorService visitorService;
 		private readonly Predicate<FileSystemInfo> searchFilter;
+
+		public bool StopOnException { get; set; } = false;
 
 		public event EventHandler<EventArgs> OnStart;
 		public event EventHandler<EventArgs> OnFinish;
@@ -22,12 +24,12 @@ namespace FileSystemVisitorLogic.Services
 
 		public FileSystemVisitor(IVisitorService visitorService)
 		{
-			this._visitorService = visitorService ?? throw new ArgumentNullException(nameof(visitorService));
+			this.visitorService = visitorService ?? throw new ArgumentNullException(nameof(visitorService));
 		}
 
 		public FileSystemVisitor(IVisitorService visitorService, Predicate<FileSystemInfo> searchFilter)
 		{
-			this._visitorService = visitorService ?? throw new ArgumentNullException(nameof(visitorService));
+			this.visitorService = visitorService ?? throw new ArgumentNullException(nameof(visitorService));
 			this.searchFilter = searchFilter;
 		}
 
@@ -54,7 +56,7 @@ namespace FileSystemVisitorLogic.Services
 				throw new ArgumentNullException(nameof(sourcePath));
 			}
 
-			if (!_visitorService.IsDirectoryExists(sourcePath))
+			if (!visitorService.IsDirectoryExists(sourcePath))
 			{
 				throw new ArgumentException($"This directory path: {sourcePath} doesn't exist");
 			}
@@ -62,64 +64,101 @@ namespace FileSystemVisitorLogic.Services
 
 		private IEnumerable<FileSystemInfo> SearchChildDirectories(DirectoryInfo path)
 		{
-			foreach (var directory in _visitorService.SearchDirectories(path))
+			IEnumerable<FileSystemInfo> directories = null;
+			try
 			{
-				if (searchFilter != null && searchFilter(directory))
-				{
-					var filterArgs = new FileSystemVisitorEventArgs(directory, "Filtered Directory Found");
-					OnFilteredDirectoryFinded?.Invoke(this, filterArgs);
+				directories = visitorService.SearchDirectories(path);
+			}
+			catch (UnauthorizedAccessException)
+			{
+				if (StopOnException)
+					throw;
+			}
+			catch(Exception)
+			{
+				if (StopOnException)
+					throw;
+			}
 
-					if (filterArgs.Status == SearchStatus.Stop)
+			if(directories != null)
+			{
+				foreach (var directory in directories)
+				{
+					if (searchFilter != null && searchFilter(directory))
+					{
+						var filterArgs = new FileSystemVisitorEventArgs(directory, "Filtered Directory Found");
+						OnFilteredDirectoryFinded?.Invoke(this, filterArgs);
+
+						if (filterArgs.Status == SearchStatus.Stop)
+							yield break;
+
+						if (filterArgs.Status != SearchStatus.Exclude)
+							yield return directory;
+					}
+
+					var findArgs = new FileSystemVisitorEventArgs(directory, "Directory Found");
+					OnDirectoryFinded?.Invoke(this, findArgs);
+
+					if (findArgs.Status == SearchStatus.Stop)
 						yield break;
 
-					if (filterArgs.Status != SearchStatus.Exclude)
+					if (findArgs.Status != SearchStatus.Exclude)
 						yield return directory;
+
+					foreach (var file in SearchFiles(directory as DirectoryInfo))
+						yield return file;
+
+					foreach (var childDirectory in SearchChildDirectories(directory as DirectoryInfo))
+						yield return childDirectory;
+
 				}
-
-				var findArgs = new FileSystemVisitorEventArgs(directory, "Directory Found");
-				OnDirectoryFinded?.Invoke(this, findArgs);
-
-				if (findArgs.Status == SearchStatus.Stop)
-					yield break;
-
-				if (findArgs.Status != SearchStatus.Exclude)
-					yield return directory;
-
-				foreach (var file in SearchFiles(directory as DirectoryInfo))
-					yield return file;
-
-				foreach (var childDirectory in SearchChildDirectories(directory as DirectoryInfo))
-					yield return childDirectory;
-				
 			}
 		}
 
 		private IEnumerable<FileSystemInfo> SearchFiles(DirectoryInfo path)
 		{
-			var files = _visitorService.SearchFiles(path);
+			IEnumerable<FileSystemInfo> files = null;
 
-			foreach (var file in files)
+			try
 			{
-				if (searchFilter != null && searchFilter(file))
-				{
-					var filterArgs = new FileSystemVisitorEventArgs(file, "Filtered File Found");
-					OnFilteredFileFinded?.Invoke(this, filterArgs);
+				files = visitorService.SearchFiles(path);
+			}
+			catch (UnauthorizedAccessException)
+			{
+				if (StopOnException)
+					throw;
+			}
+			catch (Exception)
+			{
+				if (StopOnException)
+					throw;
+			}
 
-					if (filterArgs.Status == SearchStatus.Stop)
+			if(files != null)
+			{
+				foreach (var file in files)
+				{
+					if (searchFilter != null && searchFilter(file))
+					{
+						var filterArgs = new FileSystemVisitorEventArgs(file, "Filtered File Found");
+						OnFilteredFileFinded?.Invoke(this, filterArgs);
+
+						if (filterArgs.Status == SearchStatus.Stop)
+							yield break;
+
+						if (filterArgs.Status == SearchStatus.Exclude)
+							yield return file;
+					}
+
+					var findArgs = new FileSystemVisitorEventArgs(file, "File Found");
+					OnFileFinded?.Invoke(this, findArgs);
+
+					if (findArgs.Status == SearchStatus.Stop)
 						yield break;
 
-					if (filterArgs.Status == SearchStatus.Exclude)
+					if (findArgs.Status != SearchStatus.Exclude)
 						yield return file;
 				}
-
-				var findArgs = new FileSystemVisitorEventArgs(file, "File Found");
-				OnFileFinded?.Invoke(this, findArgs);
-
-				if (findArgs.Status == SearchStatus.Stop)
-					yield break;
-
-				if (findArgs.Status != SearchStatus.Exclude)
-					yield return file;
 			}
 		}
 	}
