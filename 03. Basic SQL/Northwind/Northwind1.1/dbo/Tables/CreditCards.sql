ecbb7c691a161700303ac2a0428fca088ff8eb17
Anastasiya Petrykevich <Anastasiya.Petrykevich@thomsonreuters.com>
2020-03-17 12:19:47 +0300

    Removed

--- 03. Basic SQL/Northwind/Northwind1.1/dbo/Tables/CreditCards.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[CreditCards](
-        [CreditCardId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-        [ExpirationDate] DATETIME DEFAULT(NULL),
-        [CardHolderName] VARCHAR(200) NOT NULL,
-        [EmployeeId] INT REFERENCES [dbo].[Employees]([EmployeeID])
-        );
