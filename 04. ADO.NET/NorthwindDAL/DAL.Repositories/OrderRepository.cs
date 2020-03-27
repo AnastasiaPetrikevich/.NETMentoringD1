23c83936263043bb81993830a4c1296f831e44d4
Alex <alex.bely.420601@gmail.com>
2020-03-27 19:31:40 +0300

    CR-OrderRepository

--- 04. ADO.NET/NorthwindDAL/DAL.Repositories/OrderRepository.cs
+++ 04. ADO.NET/NorthwindDAL/DAL.Repositories/OrderRepository.cs
@@ -18,7 +18,11 @@ namespace DAL.Repositories
             ConnectionString = connectionString;
         }
 
-        public IEnumerable<Order> GetAll()
+        //NOTE: not for this task of course but for large applications pagination/filter is added for GetAll method to limit the number of retrieved documents
+        //also there are no try-catch sections so your exceptions will be thrown to BLL
+        //when I googled it I've met both opinions: that it's ok that BLL catches exceptions and that it's a wrong approach
+        //Personally I would prefer to catch SQL exceptions and throw my own exceptions
+        public IEnumerable<Order> GetAll() 
         {
             var resultOrders = new List<Order>();
 
@@ -36,12 +40,14 @@ namespace DAL.Repositories
                     {
                         while (reader.Read())
                         {
-                            var order = new Order();
+                            //var order = new Order();
 
-                            order.Set(reader);
-                            order.GetStatus();
+                            //order.Set(reader);
+                            //order.GetStatus();
 
-                            resultOrders.Add(order);
+                            //resultOrders.Add(order);'
+
+                            //need to replace with one method, that takes a reader as an Argument and returns initialized Order
                         }
                     }
                 }
@@ -52,6 +58,17 @@ namespace DAL.Repositories
 
         public Order GetOrderDetailByOrderID(int id)
         {
+            //1. Too long method. Please separate it into smaller ones. And replace Set-GetStatus with one method as I mentioned above
+            //2. Please rename the method to GetDetailedOrder or smth like that. We 
+            //3.For the task we need only product name so I don't think we need to retrieve full Product data from DB by default
+            //I would create another ProductRepository that works with product.
+            //And let a BLL decide what they want to retrieve
+            //Or add a parameter to a function like "includeProduct" and get Product if the parameter set to True
+            //4. I'm not sure but probably it's possible to use inner join for OrderDetail and product
+            //So instead of:
+            //"SELECT * FROM dbo.[Order Details] WHERE OrderID = @id"; + "SELECT * FROM dbo.Products WHERE ProductID = @id";
+            //there will be join that will give you all results without additional foreach
+            //if you have time please look at that
             using (var connection = ProviderFactory.CreateConnection())
             {
                 Order order = null;
@@ -129,6 +146,7 @@ namespace DAL.Repositories
             }
         }
 
+        //Too long method. Please separate it into smaller ones. And replace Set-GetStatus with one method as I mentioned above
         public int Add(Order order)
         {
             int newID;
@@ -140,6 +158,9 @@ namespace DAL.Repositories
 
                 using (var command = connection.CreateCommand())
                 {
+                    //why not using reflection to get Order field names?
+                    //but in general I think hardcoding insert into statement would be easier to read and maintain
+                    //you just need a placeholder to fill it with values or passing Nulls
                     var orderFields = new string[] { /*"OrderID",*/ "CustomerID", "EmployeeID", "OrderDate",
                                                      "RequiredDate", "ShippedDate", "ShipVia", "Freight",
                                                      "ShipName", "ShipAddress", "ShipCity", "ShipRegion",
@@ -202,10 +223,13 @@ namespace DAL.Repositories
             }
         }
 
+        //the same as for insert:
+        //you can have complete update-query with a placeholder that should be replaced with null or values
         public int Update(Order order)
         {
             int affected = 0;
-
+            //you retrieve order with all the details just to get the status
+            //it's better to have separate GetOrder method that will simply return an Order, without details
             var status = GetOrderDetailByOrderID(order.OrderID).Status;
             if (status == OrderStatus.New)
             {
@@ -271,12 +295,12 @@ namespace DAL.Repositories
         {
             int affected = 0;
 
-            var status = GetOrderDetailByOrderID(orderID).Status;
+            var status = GetOrderDetailByOrderID(orderID).Status;//same as above
             if (status == OrderStatus.New || status == OrderStatus.InProgress)
             {
                 using (var connection = ProviderFactory.CreateConnection())
                 {
-                    var order = new Order();
+                    var order = new Order();//not used
 
                     connection.ConnectionString = ConnectionString;
                     connection.Open();
@@ -309,7 +333,9 @@ namespace DAL.Repositories
 
                 using (var command = connection.CreateCommand())
                 {
-                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandType = CommandType.StoredProcedure; //I don't really understand why you decided to use a SP for the task :)
+                                                                        //According to the task I suppose there should be methods that update table
+                                                                        //Because SPs mentioned only for Task #7
                     command.CommandText = "SetInProgress";
 
                     var paramId = command.CreateParameter();
@@ -321,6 +347,7 @@ namespace DAL.Repositories
                     paramId = command.CreateParameter(); 
                     paramId.ParameterName = "@id";
                     paramId.Value = orderID; //I don't understand why but it's needed to set some value
+                                            // I don't know why too :)
                     paramId.Direction = ParameterDirection.Output;
                     command.Parameters.Add(paramId);
 
@@ -331,6 +358,7 @@ namespace DAL.Repositories
             return affected;
         }
 
+        //the same. I suppose it should not use SPs
         public int SetDoneStatus(int orderID)
         {
             var affected = 0;
@@ -367,12 +395,16 @@ namespace DAL.Repositories
         {
             List<CustOrdersDetail> productQuantities = new List<CustOrdersDetail>();
 
-            using (var connection = ProviderFactory.CreateConnection())
-            {
-                connection.ConnectionString = ConnectionString;
-                connection.Open();
+            //using (var connection = ProviderFactory.CreateConnection())
+            //{
+            //    connection.ConnectionString = ConnectionString;
+            //    connection.Open();
 
-                using (var command = connection.CreateCommand())
+            //    using (var command = connection.CreateCommand())
+
+            //These lines of code are used across all the methods. 
+            //Please check if it can be moved to a single method
+            //And method-specific logic can be moved to its own private methods
                 {
                     command.CommandType = CommandType.StoredProcedure;
                     command.CommandText = "CustOrdersDetail";
