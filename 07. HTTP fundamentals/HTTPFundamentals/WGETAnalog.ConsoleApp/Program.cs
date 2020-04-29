14e08b23ab5e8bc8a1775c95a45e4a1b8f80f8b1
Anastasiya Petrykevich <Anastasiya.Petrykevich@thomsonreuters.com>
2020-04-30 01:18:17 +0300

    Added HTTP (Task1)

---
+++ 07. HTTP fundamentals/HTTPFundamentals/WGETAnalog.ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using WGETAnalog.ConsoleApp.Restrictions;
+using WGETAnalog.Logic.Interfaces;
+using WGETAnalog.Logic.InterfacesImplementation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using WGETAnalog.Logic.Enums;
+
+namespace WGETAnalog.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directory = new DirectoryInfo(@"D:\HttpTest\Optika");
+            var saver = new Saver(directory);
+            var restrictions = new List<IRestrictionHelper>();
+            var logger = new Logger();
+
+            restrictions.Add(new FileRestrictionHelper("js".Select(r => "." + r)));
+            restrictions.Add(new DomainRestrictionHelper(DomainRestriction.All, new Uri("http://optikazolushka.by/")));
+
+            var downloader = new Logic.InterfacesImplementation.WGETAnalog(saver, restrictions, logger, 1);
+
+            try
+            {
+                downloader.DownloadSite("http://optikazolushka.by/");
+            }
+
+            catch (Exception ex)
+            {
+                logger.Log($"Error occured during downloading: {ex.Message}");
+            }
+        }
+    }
+}
